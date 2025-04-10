c40a6dd9fc932db802a9056015a1107932d1ce5a
sai akula <146742586+saiakulas@users.noreply.github.com>
2025-04-10 18:35:19 +0530

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', formData);
+      const res = await axios.post('https://freshnz.onrender.com/api/auth/register', formData);
       alert(res.data.message);
       navigate('/login'); // Redirect to login after successful registration
     } catch (err) {
